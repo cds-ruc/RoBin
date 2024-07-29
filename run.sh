c8c9cc3f6827c7aad8b778ee467aeea98b462e51
jscblack <jscblack@163.com>
2024-07-29 23:06:23 +0800

    fix run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,82 @@
 #!/bin/bash
+# 144 dytis bulkload patch
 
 numanode=1
 batch=1
 
 all_datasets=("covid" "osm" "fb" "genome" "planet" "linear")
 
+small_datasets=("linear" "covid" "fb" "osm")
+
+function AIOTest {
+    for dataset in ${small_datasets[@]}
+    do
+        for test_suite in 10
+        do
+            init_table_ratio=1.0
+            for ((i=1; i<=batch; i++))
+            do
+                timeout 30m numactl --cpunodebind=$numanode --membind=$numanode \
+                nice -n -10 ./build/microbench \
+                --keys_file=datasets/$dataset \
+                --keys_file_type=binary \
+                --read=0.0 --insert=0.0 \
+                --update=0.0 --scan=0.0  --delete=0.0 \
+                --test_suite=$test_suite \
+                --operations_num=0 \
+                --table_size=-1 \
+                --init_table_ratio=$init_table_ratio \
+                --thread_num=1 \
+                --memory=true \
+                --index=btree,art,alex,lipp,dytis,dili
+            done
+        done
+    
+        for test_suite in 21 22
+        do
+            for init_table_ratio in 0.0 0.005 0.01 0.025 0.05 0.1 0.25 0.5
+            do
+                for ((i=1; i<=batch; i++))
+                do
+                    timeout 30m numactl --cpunodebind=$numanode --membind=$numanode \
+                    nice -n -10 ./build/microbench \
+                    --keys_file=datasets/$dataset \
+                    --keys_file_type=binary \
+                    --read=0.0 --insert=0.0 \
+                    --update=0.0 --scan=0.0  --delete=0.0 \
+                    --test_suite=$test_suite \
+                    --operations_num=0 \
+                    --table_size=-1 \
+                    --init_table_ratio=$init_table_ratio \
+                    --thread_num=1 \
+                    --index=btree,art,alex,lipp,dytis,dili
+                done
+            done
+        done
+    
+        for test_suite in 41 42
+        do
+            for init_table_ratio in 0.005 0.01 0.025 0.05 0.1 0.25 0.5
+            do
+                for ((i=1; i<=batch; i++))
+                do
+                    timeout 30m numactl --cpunodebind=$numanode --membind=$numanode \
+                    nice -n -10 ./build/microbench \
+                    --keys_file=datasets/$dataset \
+                    --keys_file_type=binary \
+                    --read=0.0 --insert=0.0 \
+                    --update=0.0 --scan=0.0  --delete=0.0 \
+                    --test_suite=$test_suite \
+                    --operations_num=0 \
+                    --table_size=-1 \
+                    --init_table_ratio=$init_table_ratio \
+                    --thread_num=1 \
+                    --index=btree,art,alex,lipp,dytis,dili
+                done
+            done
+        done
+    done
+}
 
 function BaselineTest {
     for dataset in ${all_datasets[@]}
@@ -146,67 +218,11 @@ function ZipfSamplingTest {
     done
 }
 
-# function NormalSamplingTest {
-#     for dataset in ${all_datasets[@]}
-#     do
-#         for test_suite in 111 112
-#         do
-#             for init_table_ratio in 0.005 0.01 0.025 0.05 0.1 0.25 0.5
-#             do
-#                 for ((i=1; i<=batch; i++))
-#                 do
-#                     numactl --cpunodebind=$numanode --membind=$numanode \
-#                     nice -n -10 ./build/microbench \
-#                     --keys_file=datasets/$dataset \
-#                     --keys_file_type=binary \
-#                     --read=0.0 --insert=0.0 \
-#                     --update=0.0 --scan=0.0  --delete=0.0 \
-#                     --test_suite=$test_suite \
-#                     --operations_num=0 \
-#                     --table_size=-1 \
-#                     --init_table_ratio=$init_table_ratio \
-#                     --thread_num=1 \
-#                     --sigma_ratio=0.2 \
-#                     --index=btree,art,alex,lipp
-#                 done
-#             done
-#         done
-#     done
-# }
-
-# function ShiftTest {
-#     all_datasets=("covid" "libio" "osm" "fb")
-#     for bulk_dataset in ${all_datasets[@]}
-#     do
-#         for insert_dataset in ${all_datasets[@]}
-#         do
-#             if [ $bulk_dataset == $insert_dataset ]; then
-#                 continue
-#             fi
-#             test_suite=10
-#             init_table_ratio=0.5
-#             for ((i=1; i<=batch; i++))
-#             do
-#                 numactl --cpunodebind=$numanode --membind=$numanode \
-#                 nice -n -10 ./build/microbench \
-#                 --keys_file=datasets/$bulk_dataset \
-#                 --backup_keys_file=datasets/$insert_dataset \
-#                 --keys_file_type=binary \
-#                 --read=0.0 --insert=0.0 \
-#                 --update=0.0 --scan=0.0  --delete=0.0 \
-#                 --test_suite=$test_suite \
-#                 --operations_num=0 \
-#                 --table_size=-1 \
-#                 --init_table_ratio=$init_table_ratio \
-#                 --thread_num=1 \
-#                 --index=alex,lipp
-#             done
-#         done
-#     done
-# }
-
 # select test from input
 case $1 in
+    AIOTest)
+        AIOTest
+        ;;
     BaselineTest)
         BaselineTest
         ;;
@@ -222,36 +238,7 @@ case $1 in
     ZipfSamplingTest)
         ZipfSamplingTest
         ;;
-
     *)
-        echo "Usage: $0 {BaselineTest|UniformSamplingTest|IntervalSampingTest|SubstringSamplingTest|ZipfSamplingTest}"
+        echo "Usage: $0 {AIOTest|BaselineTest|UniformSamplingTest|IntervalSampingTest|SubstringSamplingTest|ZipfSamplingTest}"
         exit 1
 esac
-
-
-# # Dive into the fb/alex
-# for dataset in "fb"
-# do
-#     for test_suite in 99 2 4
-#     do
-#         for init_table_ratio in 0.005 0.01 0.025 0.05 0.1 0.25 0.5
-#         do
-#             for ((i=1; i<=batch; i++))
-#             do
-#                 nice -n -10 ./build/microbench \
-#                 --keys_file=datasets/$dataset \
-#                 --keys_file_type=binary \
-#                 --read=0.0 --insert=0.0 \
-#                 --update=0.0 --scan=0.0  --delete=0.0 \
-#                 --test_suite=$test_suite \
-#                 --operations_num=0 \
-#                 --table_size=-1 \
-#                 --init_table_ratio=$init_table_ratio \
-#                 --thread_num=1 \
-#                 --memory=true \
-#                 --dump_bulkload=true \
-#                 --index=alex
-#             done
-#         done
-#     done
-# done
