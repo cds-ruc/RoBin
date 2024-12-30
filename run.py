490c369926bb00e966366caec2095b7e85e12b19
tong1heng <yihengtong@outlook.com>
2024-12-30 15:27:04 +0800

    fix script for preload

--- run.py
+++ run.py
@@ -65,6 +65,7 @@ def main():
         "--hardness_statistic", required=False, default=False, help="hardness statistic"
     )
     parser.add_argument("--taskset", required=False, help="taskset")
+    parser.add_argument("--preload_suite", required=False, default=0, help="preload suite")
 
     args = parser.parse_args()
 
@@ -146,7 +147,7 @@ def main():
     elif args.partition_method == "model":
         partition_arg = f" --partition_num={args.partition_num} --partition_method=model"
     
-    command = f"{numactl_arg} {args.taskset} ./build/microbench --keys_file=datasets/{args.dataset} {keys_path2_arg}  --keys_file_type=binary {hardness_statistic_arg} --read=0.0 --insert=0.0 --update=0.0 --scan=0.0 --delete=0.0 --test_suite={test_suite} --operations_num=0 --table_size=-1 --init_table_ratio={init_table_ratio} --del_table_ratio=0.0 --sample_round={args.sampling_round} --thread_num={args.concurrency} {partition_arg} --index={args.index}"
+    command = f"{numactl_arg} {args.taskset} ./build/microbench --keys_file=datasets/{args.dataset} {keys_path2_arg}  --keys_file_type=binary {hardness_statistic_arg} --read=0.0 --insert=0.0 --update=0.0 --scan=0.0 --delete=0.0 --test_suite={test_suite} --operations_num=0 --table_size=-1 --init_table_ratio={init_table_ratio} --del_table_ratio=0.0 --sample_round={args.sampling_round} --thread_num={args.concurrency} {partition_arg} --index={args.index} --preload_suite={args.preload_suite}"
     print(f"Running command: {command}")
     try:
         proc = subprocess.Popen(command.split())
--- script/expr.sh
+++ script/expr.sh
@@ -257,64 +257,67 @@ function test_preload_single_thread() {
   concurrency_options=(1)
   numanode=1 # 145
   export numanode=$numanode
+  preload_suites=(113 114 123 124 133 134 143 144)
 
   # Iterate over all combinations of parameters
-  for index in "${index_options[@]}"; do
-    for dataset in "${dataset_options[@]}"; do
-      for sampling_method in "${sampling_method_options[@]}"; do
-        for bulkload_size in "${bulkload_size_options[@]}"; do
-          for insert_pattern in "${insert_pattern_options[@]}"; do
-            for concurrency in "${concurrency_options[@]}"; do
-              if [ "$bulkload_size" = "200000000" ] && [ "$sampling_method" != "full" ]; then
-                # Skip bulkload size 200M with sampling method other than full
-                continue
-              fi
-              
-              if [ "$sampling_method" = "full" ] && { [ "$bulkload_size" != "200000000" ] || [ "$insert_pattern" != "sorted" ]; }; then
-                # Skip full sampling with bulkload sizes other than 200M and insert patterns must be sorted
-                continue
-              fi
-
-              if [ "$bulkload_size" = "0" ] && [ "$sampling_method" != "uniform" ]; then
-                # Skip bulkload size 0 with sampling_method_options other than uniform
-                continue
-              fi
-
-              # Construct the base command
-              cmd="python3 run.py --index=$index --dataset=$dataset --concurrency=$concurrency"
-              
-              # Add optional parameters
-              if [ -n "$sampling_method" ]; then
-                cmd+=" --sampling_method=$sampling_method"
-              fi
-              if [ -n "$bulkload_size" ]; then
-                cmd+=" --bulkload_size=$bulkload_size"
-              fi
-              if [ -n "$insert_pattern" ]; then
-                cmd+=" --insert_pattern=$insert_pattern"
-              fi
-              
-              # Determine taskset CPUs based on concurrency, starting from CPU 2
-              if (( concurrency > 0 )); then
-                # Generate a comma-separated list from 2 up to (2 + concurrency - 1)
-                if (( numanode == 0 )); then
-                  end_cpu=$((2 + concurrency - 1))
-                  cmd+=" --taskset=2-$end_cpu"
-                else
-                  end_cpu=$((20 + concurrency - 1))
-                  cmd+=" --taskset=20-$end_cpu"
+  for preload_suite in "${preload_suites[@]}"; do
+    for index in "${index_options[@]}"; do
+      for dataset in "${dataset_options[@]}"; do
+        for sampling_method in "${sampling_method_options[@]}"; do
+          for bulkload_size in "${bulkload_size_options[@]}"; do
+            for insert_pattern in "${insert_pattern_options[@]}"; do
+              for concurrency in "${concurrency_options[@]}"; do
+                if [ "$bulkload_size" = "200000000" ] && [ "$sampling_method" != "full" ]; then
+                  # Skip bulkload size 200M with sampling method other than full
+                  continue
                 fi
-              fi
+                
+                if [ "$sampling_method" = "full" ] && { [ "$bulkload_size" != "200000000" ] || [ "$insert_pattern" != "sorted" ]; }; then
+                  # Skip full sampling with bulkload sizes other than 200M and insert patterns must be sorted
+                  continue
+                fi
 
-              # If mix the read-write
-              if ((mixed_rw)); then
-                cmd+=" --mixed_rw=1"
-              fi
-              
-              # Run the command 3 times
-              for epoch in $(seq 1 $epoch_limit); do
-                echo "Running command (epoch $epoch): $cmd"
-                eval "$cmd >> run.log"
+                if [ "$bulkload_size" = "0" ] && [ "$sampling_method" != "uniform" ]; then
+                  # Skip bulkload size 0 with sampling_method_options other than uniform
+                  continue
+                fi
+
+                # Construct the base command
+                cmd="python3 run.py --index=$index --dataset=$dataset --concurrency=$concurrency --preload_suite=$preload_suite"
+                
+                # Add optional parameters
+                if [ -n "$sampling_method" ]; then
+                  cmd+=" --sampling_method=$sampling_method"
+                fi
+                if [ -n "$bulkload_size" ]; then
+                  cmd+=" --bulkload_size=$bulkload_size"
+                fi
+                if [ -n "$insert_pattern" ]; then
+                  cmd+=" --insert_pattern=$insert_pattern"
+                fi
+                
+                # Determine taskset CPUs based on concurrency, starting from CPU 2
+                if (( concurrency > 0 )); then
+                  # Generate a comma-separated list from 2 up to (2 + concurrency - 1)
+                  if (( numanode == 0 )); then
+                    end_cpu=$((2 + concurrency - 1))
+                    cmd+=" --taskset=2-$end_cpu"
+                  else
+                    end_cpu=$((20 + concurrency - 1))
+                    cmd+=" --taskset=20-$end_cpu"
+                  fi
+                fi
+
+                # If mix the read-write
+                if ((mixed_rw)); then
+                  cmd+=" --mixed_rw=1"
+                fi
+                
+                # Run the command 3 times
+                for epoch in $(seq 1 $epoch_limit); do
+                  echo "Running command (epoch $epoch): $cmd"
+                  eval "$cmd >> run.log"
+                done
               done
             done
           done
@@ -448,6 +451,6 @@ function test_index_partition_single_thread() {
 }
 # test_shift_multi_thread;
 # test_shift_single_thread;
-# test_preload_single_thread;
+test_preload_single_thread;
 # test_zipfian_mitigation_single_thread;
-test_index_partition_single_thread;
+# test_index_partition_single_thread;
